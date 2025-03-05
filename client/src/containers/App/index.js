3f990d9ab0073fa703dfa261b0ff22d15880e2ad
James Geib <jpgeib2@gmail.com>
2025-03-05 11:08:16 -0500

    added initial folders and files to pages, containers, and components in src folder

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
