46215b1957167dd5359ce3c176b2c3f6e90322c4
James Geib <jpgeib2@gmail.com>
2025-03-20 19:45:58 -0400

    created initial structures for frontend components and pages

---
+++ client/src/components/AnnouncementBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnnouncementBar = () => {
+    return (
+        <>
+            <h1>Announcement Bar</h1>
+        </>
+    );
+};
+
+export default AnnouncementBar;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const About = () => {
+const Navbar = () => {
     return (
         <>
-            <h1>About Page</h1>
+            <h1>Navbar</h1>
         </>
     );
 };
 
-export default About;
+export default Navbar;
