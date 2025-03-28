49f53e5448f42cb8f92e69635af07006e45788f3
James Geib <jpgeib2@gmail.com>
2025-03-27 20:26:44 -0400

    constructed database config file

---
+++ config/index.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.NODE_ENV === "production" ? process.env.PROD_DB_HOST : process.env.DEV_DB_HOST,
+    user: process.env.NODE_ENV === "production" ? process.env.PROD_DB_USER : process.env.DEV_DB_USER,
+    password: process.env.NODE_ENV === "production" ? process.env.PROD_DB_PASSWORD : process.env.DEV_DB_PASSWORD,
+    database: process.env.NODE_ENV === "production" ? process.env.PROD_DB_NAME : process.env.DEV_DB_NAME
+});
+
+console.log(
+    db.config.host,
+    db.config.user
+);
+
+module.exports = db;
