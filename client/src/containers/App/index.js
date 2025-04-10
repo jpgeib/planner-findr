08a37bcf171f6018d5cc7ecf5b111b5534ff2d5b
James Geib <jpgeib2@gmail.com>
2025-04-09 21:37:48 -0400

    created additional App routes for other pages in App container

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -2,15 +2,35 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
+import Register from "../../pages/Register";
+import Login from "../../pages/Login";
+import ForgotPassword from "../../pages/ForgotPassword";
+import ResetPassword from "../../pages/ResetPassword";
+import Providers from "../../pages/Providers";
+import SingleProvider from "../../pages/SingleProvider";
+import Reviews from "../../pages/Reviews";
+import SingleReview from "../../pages/SingleReview";
+import Write from "../../pages/Write";
+import Edit from "../../pages/Edit";
 import Contact from "../../pages/Contact";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/forgot" element={<ForgotPassword />} />
+        <Route exact path="/reset/:id/:token" element={<ResetPassword />} />
+        <Route exact path="/providers" element={<Providers />} />
+        <Route exact path="/provider/:id" element={<SingleProvider />} />
+        <Route exact path="/reviews" element={<Reviews />} />
+        <Route exact path="/review/:id" element={<SingleReview />} />
+        <Route exact path="/write" element={<Write />} />
+        <Route exact path="/edit/:id" element={<Edit />} />
+        <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
