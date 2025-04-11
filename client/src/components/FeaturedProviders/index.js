acf5e4f98971ff7f5cccadd9f17ef4537ffb1650
James Geib <jpgeib2@gmail.com>
2025-04-11 17:31:17 -0400

    implemented initial structure for FeaturedProviders

--- client/src/components/FeaturedProviders/index.js
+++ client/src/components/FeaturedProviders/index.js
@@ -5,7 +5,13 @@ import "./style.css";
 const FeaturedProviders = () => {
     return (
         <>
-            <h1>Featured Providers</h1>
+            <div id="featured-providers-container">
+                <h1 id="featured-providers-header">Featured Providers</h1>
+                <h4 id="featured-providers-subheader">Explore our top-rated service providers</h4>
+                <div id="featured-providers">
+                    {/* Featured providers will be displayed here */}
+                </div>
+            </div>
         </>
     );
 };
