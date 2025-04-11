54f995e6df80a00a598b6006ef65a003a57a5585
James Geib <jpgeib2@gmail.com>
2025-04-11 10:33:48 -0400

    implemented Navbar and additional page routes into App container

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
 import Register from "../../pages/Register";
 import Login from "../../pages/Login";
 import ForgotPassword from "../../pages/ForgotPassword";
 import ResetPassword from "../../pages/ResetPassword";
+import Profile from "../../pages/Profile";
 import Providers from "../../pages/Providers";
 import SingleProvider from "../../pages/SingleProvider";
 import Reviews from "../../pages/Reviews";
@@ -13,10 +15,15 @@ import SingleReview from "../../pages/SingleReview";
 import Write from "../../pages/Write";
 import Edit from "../../pages/Edit";
 import Contact from "../../pages/Contact";
+import SingleEvent from "../../pages/SingleEvent";
+import Events from "../../pages/Events";
+import Reservations from "../../pages/Reservations";
+
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
@@ -24,6 +31,11 @@ const App = () => {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/forgot" element={<ForgotPassword />} />
         <Route exact path="/reset/:id/:token" element={<ResetPassword />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/events" element={<Events />} />
+        <Route exact path="/event/:id" element={<SingleEvent />} />
+        <Route exact path="/reservations" element={<Reservations />} />
+        <Route exact path="/reservation/:id" element={<SingleReservation />} />
         <Route exact path="/providers" element={<Providers />} />
         <Route exact path="/provider/:id" element={<SingleProvider />} />
         <Route exact path="/reviews" element={<Reviews />} />
