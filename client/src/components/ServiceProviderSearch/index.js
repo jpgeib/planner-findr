f8840eca1baaaf1f795b8083f7e96dd87fd0e513
James Geib <jpgeib2@gmail.com>
2025-04-11 10:01:38 -0400

    created elements for ServiceSearchProvider component

--- client/src/components/ServiceProviderSearch/index.js
+++ client/src/components/ServiceProviderSearch/index.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Header, Button } from "semantic-ui-react";
 
 import "./style.css";
 
 const ServiceSearchProvider = () => {
     return (
         <>
-            <h1>Service Search Provider</h1>
+            <div id="service-provider-search-container">
+                <div id="service-search-provider">
+                    <Header id="service-provider-search-header" as="h1">Service Search Provider</Header>
+                    <Header as="h4">Select from a wide range of service providers for your event needs</Header>
+                    <input />
+                    <Button id="service-provider-search-reset-filter-btn" content="Reset Filters" />
+                    <Button id="service-provider-search-filter-btn" content="Filter" />
+                </div>
+            </div>
+
         </>
     );
 };
