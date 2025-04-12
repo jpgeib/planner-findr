e3fd9b1d09e2edb4a9172fd9eafa8359f40be4c0
James Geib <jpgeib2@gmail.com>
2025-04-12 00:44:05 -0400

    created Logout and DeleteReview modals

---
+++ client/src/components/DeleteReviewModal/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button, Header, Modal, Icon } from "semantic-ui-react";
+
+import "./style.css";
+
+const DeleteReviewModal = (props) => {
+
+    const { handleDelete, screenWidth } = props;
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={
+                <Button id="review-delete-btn" icon>
+                    {screenWidth <= 516 ? "Delete " : null}
+                    <Icon name="x" />
+                </Button>
+            }
+        >
+            <Modal.Header>Delete Review</Modal.Header>
+            <Modal.Content>
+                <Modal.Description>
+                    <Header>Are you sure you want to delete this review?</Header>
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button color='black' onClick={() => setOpen(false)}>
+                    Cancel
+                </Button>
+                <Button
+                    content="Delete"
+                    labelPosition='right'
+                    icon='x'
+                    onClick={handleDelete}
+                    negative
+                />
+            </Modal.Actions>
+        </Modal>
+    );
+};
+
+export default DeleteReviewModal;
