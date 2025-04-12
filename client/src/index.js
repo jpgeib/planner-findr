909f00dd377d6199cd750399629b3062be5e1d51
James Geib <jpgeib2@gmail.com>
2025-04-12 10:06:03 -0400

    implemented scrollToTop into root index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "./context/authContext";
 import App from "./containers/App";
+import ScrollToTop from "./components/ScrollToTop";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Router>
+        <ScrollToTop />
         <AuthContextProvider>
             <App />
         </AuthContextProvider>
