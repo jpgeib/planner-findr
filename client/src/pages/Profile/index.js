6ed71d7b1af1a3610d461e5dbb1b09c40eb44c56
James Geib <jpgeib2@gmail.com>
2025-04-12 23:22:30 -0400

    got started on Profile page structure

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Header, Rating, Image, Button, Icon } from "semantic-ui-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import ReservationRequest from "../../components/ReservationRequest";
+// import DeleteProviderModal from "../../components/DeleteProviderModal";
+import { axiosInstance } from "../../utils/api";
 
 const Profile = (props) => {
+
+    const { currentUser, notAuthorized } = props.auth;
+    const { screenWidth } = props;
+    const [provider, setProvider] = useState({});
+    const [reviews, setReviews] = useState([]);
+
     return (
         <>
-            <h1>Profile Page</h1>
+            <div id="profile-container">
+                <div id="profile">
+                    <div className="profile-col">
+                        <Image />
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
